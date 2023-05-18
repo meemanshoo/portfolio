5876f14dc7c5e07fbe39ffe046977ee8704d5fdd
@Meemanshoo007 <meemanshoo@gmail.com>
2023-05-18 16:52:00 +0530

    test

---
+++ js/data.js
@@ -0,0 +1,91 @@
+window.onload = function() {
+    //header
+     var hellow = "Hello!";
+     var iam = "I'm";
+     var iama = "I'm a";
+     var name = "Rituraj";
+     var work = "A Freelance Web Designer";
+     var work2 = "web designer";
+     var location = "based in India";
+
+     var btn1 = "Hire me";
+     var btn2 = "My works";
+
+    //about heading
+    var lightBackAbout = "About";
+    var aboutMe = "About Me";
+    var aboutMeContent = "A small river named Duden flows by their place and supplies it with the necessary regelialia.";
+
+    //about details
+    var name = "Name:";
+    var nameVal = "Rituraj";
+    var dob = "Date of birth:";
+    var dobVal = "March 28, 2004";
+    var add = "Address:";
+    var addVal = "Uttarpradesh India";
+    var zip = "Zip code:";
+    var zipVal = "232101";
+    var email = "Email:";
+    var emailVal = "rituraj@gmail.com"
+    var phone = "Phone:";
+    var phoneVal = "+91 9415201379";
+
+    var projects = "120";
+    var projectsComplete = "Project complete";
+    var downloadCv = "Download CV";
+
+    //resume
+    var resumeBackLight = "Resume";
+    var resume = "Resume";
+    var resumeDesc = "A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.";
+
+    //resume work done list 
+    var resumeData = [{
+        "year":"2014-2015","done":"Master Degree of Design","subDone":"CAMBRIDGE UNIVERSITY","desc":"A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth."
+    }];
+
+    //services
+    var lightBackServices = "Services";
+    var services = "Services";
+    var serviceDesc = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia";
+
+    //services list
+    var serviceData = [
+        {
+            "name":"Web Design"
+        }
+    ];
+
+    //myskills
+    var lightBackSkills = "Skills";
+    var skills = "My Skills";
+    var skillsDesc = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia";
+
+    //myskills list
+    var skillsData = [
+        {
+            "name":"Photoshop",
+            "value":"90"
+        }
+    ];
+
+    //our Projects 
+    var lightBackAProject = "Projects";
+    var projects = "Our Projects";
+    var projectsDesc = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia";
+
+    // project list
+    var ProjectsData = [
+        {
+            "heading":"Branding &amp; Illustration Design",
+            "subHeadings": "Web Design"
+        }
+    ];
+
+    // blog
+
+    var blogLightBack = "Blog";
+    var blogDesc = "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia";
+
+
+  };
